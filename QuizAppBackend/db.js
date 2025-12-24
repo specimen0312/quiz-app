a20dd108a6c349011a34b0e6e9ab7d98bdb8fd44
ab0312 <akbodkhe03@gmail.com>
2025-12-23 19:56:11 -0800

    Update db.js

--- QuizAppBackend/db.js
+++ QuizAppBackend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB(uri) {
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri);
-  console.log("✅ MongoDB connected");
+  console.log("MongoDB connected");
 }
 
 module.exports = connectDB;
